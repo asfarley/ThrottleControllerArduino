7aa59c8f855af4d74a2b453b196316f1e1f50306
Alexander Farley <alexander.s.farley@gmail.com>
2023-12-07 11:37:10 -0700

    Increase update rate.

--- throttle_blink.ino
+++ throttle_blink.ino
@@ -118,10 +118,11 @@ void loop() {
 #endif
    }
 
-  digitalWrite(A0, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(A0, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);  
+  delay(50);
+  //digitalWrite(A0, HIGH);  // turn the LED on (HIGH is the voltage level)
+  //delay(1000);                      // wait for a second
+  //digitalWrite(A0, LOW);   // turn the LED off by making the voltage LOW
+  //delay(1000);  
 
 }
 
